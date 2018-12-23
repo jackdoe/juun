ece8ee08fa6d795510d750fe11e5bba40608469f
borislav nikolov <jack@sofialondonmoskva.com>
2018-12-23 18:19:18 +0100

    bump

--- brew/juun.rb
+++ brew/juun.rb
@@ -1,9 +1,9 @@
 class Juun < Formula
   desc "juun - bash/zsh history manager and search"
   homepage "https://github.com/jackdoe/juun"
-  url "https://github.com/jackdoe/juun/archive/d1975731f624cd902e0f188cf8e4a5343fd65308.zip"
-  version "0.1"
-  sha256 "3e626f9d0a5fbd026e300c3965f83df4aa09095f"
+  url "https://github.com/jackdoe/juun/archive/f98dee2b47b7c31cd48669a285ae0169acbea04e.zip"
+  version "0.2"
+  sha256 "b04d052fc8744b9fe3ea71c400700aa5cbedeb52"
   depends_on "golang" => :build, "make" => :build
   def install
     system "make", "install" 
